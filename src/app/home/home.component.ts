74e1df7e2118d9c080ad0c6899d09cc22f6b30e9
Ed <Eduard.Jooste@absa.africa>
2021-02-10 06:52:07 +0200

    first commit

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,57 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { Entry } from '../app.model';
+import { AppService } from '../app.service';
+
+@Component({
+  selector: 'app-entries',
+  templateUrl: './entries.component.html',
+  styleUrls: ['./entries.component.css'],
+  providers: [AppService]
+})
+export class EntriesComponent implements OnInit {
+  loadedEntries: Entry[] = [];
+  isFetching = false;
+  error = null;
+
+  constructor(private http: HttpClient, private appService: AppService) {}
+
+  ngOnInit() {
+    this.isFetching = true;
+    this.appService.fetchEntries().subscribe(
+      entries => {
+        this.isFetching = false;
+        this.loadedEntries = entries;
+      },
+      error => {
+        this.isFetching = false;
+        this.error = error.message;
+      }
+    );
+  }
+
+  onAddEntry(postData: Entry) {
+    this.appService.addEntry(postData.name, postData.phoneNumber);
+  }
+
+  onFetchEntries() {
+    this.isFetching = true;
+    this.appService.fetchEntries().subscribe(
+      entries => {
+        this.isFetching = false;
+        this.loadedEntries = entries;
+      },
+      error => {
+        this.isFetching = false;
+        this.error = error.message;
+        console.log(error);
+      }
+    );
+  }
+
+  onHandleError() {
+    this.error = null;
+  }
+}
+
